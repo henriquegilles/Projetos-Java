17161ab2ce8a35d4fe07e26ac313856525c92dfe
Henrique Gilles <henriquegillesrocha@gmail.com>
2020-09-23 10:57:21 -0300

    Media Ponderada

---
+++ Projetos-Java/mp.java
@@ -0,0 +1,22 @@
+package faculdade;
+import java.util.Scanner;
+public class mp {
+    public static void main(final String[] args) {
+        final Scanner scan = new Scanner(System.in);
+       int n1, n2, n3;
+     
+       double media;
+       
+
+
+       System.out.println("Insira a sua nota "); 
+       n1= scan.nextInt();
+       n2= scan.nextInt();
+       n3= scan.nextInt();
+
+       media =(n1*2+n2*3+n3*5)/10;
+
+       System.out.println("Sua media é : " +media);
+
+    }
+}
